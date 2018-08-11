8768e8cbfc52cbe28f315797880db3a530fdfe9d
Yanglin Zhao <yz1876@gmail.com>
2018-08-11 09:46:34 -0400

    Send real data in searchProducts query

--- server/graphql.py
+++ server/graphql.py
@@ -1,14 +1,26 @@
 import graphene
 
+from server import walmart
+
+
+class Product(graphene.ObjectType):
+    """Walmart product
+    """
+
+    item_id = graphene.ID()
+
 
 class Query(graphene.ObjectType):
     """Root query
     """
 
-    foo = graphene.List(graphene.Int)
+    search_products = graphene.List(Product, query=graphene.String())
 
-    def resolve_foo(self, info, **kwargs):
-        return [1, 2, 3]
+    def resolve_search_products(self, info, **kwargs):
+        query = kwargs["query"]
+        products = walmart.get_search_products(query)
+        products_attrs = [{"item_id": p["itemId"]} for p in products["items"]]
+        return [Product(**attrs) for attrs in products_attrs]
 
 
 schema = graphene.Schema(query=Query)
