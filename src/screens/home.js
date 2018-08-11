f595fd83d9e8473d7410dc5e0c5af5ff0a3d8082
Yanglin Zhao <yz1876@gmail.com>
2018-08-11 19:07:30 -0400

    Implement basic search form on home page

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,52 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
-function HomeScreen() {
+class SearchInput extends React.Component {
+  state = {
+    searchQuery: ""
+  };
+
+  handleChange = e => {
+    this.setState({ searchQuery: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { searchQuery } = this.state;
+    const { history } = this.props;
+
+    const newPath = `/search?q=${searchQuery}`;
+    history.push(newPath);
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Search:
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
+function HomeScreen(props) {
+  const { history } = props;
+
   return (
     <div className="HomeScreen">
       <h1>Home</h1>
+
+      <SearchInput history={history} />
     </div>
   );
 }
 
-export default HomeScreen;
+export default withRouter(HomeScreen);
