13d0fa0713d492f7fa7224d8d201300613090bdb
Yanglin Zhao <yz1876@gmail.com>
2018-08-11 10:21:55 -0400

    Add recommended products query

--- server/graphql.py
+++ server/graphql.py
@@ -15,16 +15,22 @@ class Product(graphene.ObjectType):
     medium_image = graphene.String()
     large_image = graphene.String()
 
+    recommended_products = graphene.List(lambda: Product)
+
+    def resolve_recommended_products(self, info, **kwargs):
+        recommended = walmart.get_product_recommendations(self.item_id)
+        return [Product.from_api_object(p) for p in recommended]
+
     @classmethod
     def from_api_object(cls, api_object):
         return cls(
-            item_id=api_object["itemId"],
-            name=api_object["name"],
-            sale_price=api_object["salePrice"],
-            short_description=api_object["shortDescription"],
-            thumb_image=api_object["thumbnailImage"],
-            medium_image=api_object["mediumImage"],
-            large_image=api_object["largeImage"],
+            item_id=api_object.get("itemId"),
+            name=api_object.get("name"),
+            sale_price=api_object.get("salePrice"),
+            short_description=api_object.get("shortDescription"),
+            thumb_image=api_object.get("thumbnailImage"),
+            medium_image=api_object.get("mediumImage"),
+            large_image=api_object.get("largeImage"),
         )
 
 
