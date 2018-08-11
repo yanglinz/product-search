ca4c0ef2e63eab7bd4607aa7bf281943ec2e8900
Yanglin Zhao <yz1876@gmail.com>
2018-08-11 10:14:51 -0400

    Add more fields to product schema

--- server/graphql.py
+++ server/graphql.py
@@ -4,10 +4,28 @@ from server import walmart
 
 
 class Product(graphene.ObjectType):
-    """Walmart product
+    """Walmart product schema
     """
 
     item_id = graphene.ID()
+    name = graphene.String()
+    sale_price = graphene.String()
+    short_description = graphene.String()
+    thumb_image = graphene.String()
+    medium_image = graphene.String()
+    large_image = graphene.String()
+
+    @classmethod
+    def from_api_object(cls, api_object):
+        return cls(
+            item_id=api_object["itemId"],
+            name=api_object["name"],
+            sale_price=api_object["salePrice"],
+            short_description=api_object["shortDescription"],
+            thumb_image=api_object["thumbnailImage"],
+            medium_image=api_object["mediumImage"],
+            large_image=api_object["largeImage"],
+        )
 
 
 class Query(graphene.ObjectType):
