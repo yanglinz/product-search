ddbe1e5db9f5232c8ef444b12cd5668bcf37e6a7
Yanglin Zhao <yz1876@gmail.com>
2018-08-11 15:14:11 -0400

    Add real query to product detail screen

--- src/screens/product.js
+++ src/screens/product.js
@@ -1,11 +1,41 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
-function ProductScreen() {
+const PRODUCT_DETAIL_QUERY = gql`
+  query ProductDetailQuery($itemId: ID!) {
+    productDetail(id: $itemId) {
+      itemId
+      name
+    }
+  }
+`;
+
+function ProductScreen(props) {
+  const { match } = props;
+  const { itemId } = match.params;
+
   return (
     <div className="ProductScreen">
       <h1>Product</h1>
+
+      <Query query={PRODUCT_DETAIL_QUERY} variables={{ itemId }}>
+        {({ loading, error, data }) => {
+          const { productDetail } = data || {};
+
+          if (loading) return <p>Loading...</p>;
+          if (error) return <p>Error :(</p>;
+
+          return (
+            <div>
+              <h1>{productDetail.name}</h1>
+            </div>
+          );
+        }}
+      </Query>
     </div>
   );
 }
 
-export default ProductScreen;
+export default withRouter(ProductScreen);
