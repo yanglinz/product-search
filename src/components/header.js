802fd2e170a9626b9018bda65d3b224bc88fa95b
Yanglin Zhao <yz1876@gmail.com>
2018-08-13 22:07:28 -0400

    Add github logo to source link

--- src/components/header.js
+++ src/components/header.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import logoSmall from "../assets/logo-small.svg";
+import githubLogo from "../assets/github.svg";
 import "./header.css";
 
 function Header(props) {
   const ghLink = "https://github.com/yanglinz/product-search";
   return (
     <div className="Header container-fluid">
-      <div className="row">
+      <div className="row middle-xs">
         <div className="Header-brand col-xs-3">
           <h1 className="Header-logo">
             <Link to="/">
@@ -20,7 +21,12 @@ function Header(props) {
         <div className="Header-main col-xs-6">{props.children}</div>
 
         <div className="Header-github col-xs-3">
-          <a href={ghLink}>Source</a>
+          <a href={ghLink} target="_blank">
+            <span className="Header-github-text">Source</span>
+            <span className="Header-github-logo">
+              <img src={githubLogo} height="11" />
+            </span>
+          </a>
         </div>
       </div>
     </div>
