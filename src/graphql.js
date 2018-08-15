73360c8860529b7fcc6e6c44d5c1068bbdca60fb
Yanglin Zhao <yz1876@gmail.com>
2018-08-15 12:34:10 -0400

    Point to live graphql server in production

--- src/graphql.js
+++ src/graphql.js
@@ -1,7 +1,10 @@
 import ApolloClient from "apollo-boost";
 
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:8000/graphql/"
+  uri:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8000/graphql/"
+      : "https://product-search-walmart.herokuapp.com/graphql/"
 });
 
 export default apolloClient;
