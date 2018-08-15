5f01e4fd0c1cc58f6dc7bb75a3af763a985cbcdc
Yanglin Zhao <yz1876@gmail.com>
2018-08-15 13:05:20 -0400

    Add a controlled input example to storybook

--- src/components/search-input.story.js
+++ src/components/search-input.story.js
@@ -4,6 +4,12 @@ import { action } from "@storybook/addon-actions";
 
 import SearchInput from "./search-input";
 
-storiesOf("SearchInput", module).add("default", () => (
-  <SearchInput onSubmit={action("onSubmit")} />
-));
+storiesOf("SearchInput", module)
+  .add("default", () => <SearchInput onSubmit={action("onSubmit")} />)
+  .add("with controlled inputs", () => (
+    <SearchInput
+      value="Hello world!"
+      onChange={action("onChange")}
+      onSubmit={action("onSubmit")}
+    />
+  ));
